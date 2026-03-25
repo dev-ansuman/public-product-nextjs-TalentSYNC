f6bfbffafeff0772f50ee8a583daf8da1f512e2b
Ansuman <ansumanp@mindfiresolutions.com>
2026-03-25 18:24:03 +0530

    feat: setup strapi CMS

---
+++ talentsync-cms/config/plugins.ts
@@ -0,0 +1,6 @@
+import type { Core } from '@strapi/strapi';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+
+export default config;
